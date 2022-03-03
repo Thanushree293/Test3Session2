8d1c1f151e5cbd8d7c68cff174eab361886a1f19
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-03-03 08:49:34 +0000

    to add 2 fractions

--- p1final.c
+++ p1final.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct _fraction Fraction;
+int find_gcd(int a, int b)
+{
+  int t;
+  while(b!=0)
+    {
+      t=b;
+      b=a%b;
+      a=t;
+    }
+  return a;
+}
+int main()
+{
+  printf("%d\n",find_gcd(16,24));
+}
+//Fraction input_fraction();
+//Fraction add_fractions(Fraction f1, Fraction f2)
+//void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum)
