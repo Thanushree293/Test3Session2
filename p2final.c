01a2d5b38b90d60f8933580577df48d05191b702
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-03-15 17:42:51 +0000

    ncr

--- p2final.c
+++ p2final.c
@@ -1,49 +1,21 @@
 #include<stdio.h>
-#include<math.h>
 struct _fraction
 {
-int num,den;
+  int num,den;
 };
 typedef struct _fraction Fraction;
-
-Fraction input_fraction()
-{
-Fraction f;
-  printf("enter the number\n");
-  scanf("%d %d",&f.num,&f.den);
- }
-Fraction smallest_fraction(Fraction f1, Fraction f2, Fraction f3)
-{
-  int x=f1.den * f2.den * f3.den;
-  f1.num=f1.num*(x/f1.den);
-  f2.num=f2.num*(x/f2.den);
-  f3.num=f3.num*(x/f3.den);
-  if(f1.num<f2.num && f1.num<f3.num)
-    return f1.num;
-  else if(f2.num<f3.num)
-    return f2.num;
-      else
-    return f3.num;
-  }
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction smallest)
-{
-  if(smallest==f1.num)
-  printf("%f is smallest fraction\n",f1);
-else if(smallest==f2.num)
-{
-  printf("%f is smallest fraction\n",f2);
-  }
-  else
-  printf("%f is smallest fraction\n",f3);
-  }
+  int gcd(int a,int b)
+  {
+    int t;
+    while (b != 0)
+      {
+        t = b;
+        b = a%b;
+        a = t;
+      }
+    return a;
+}
 int main()
 {
-  Fraction smallest,f1,f2,f3;
-  f1=input_fraction();
-  f2=input_fraction();
-  f3=input_fraction();
-  smallest= smallest_fraction(f1,f2,f3);
-  output(f1,f2,f3,smallest);
-  return 0;
-  
+  printf("%d\n",gcd(16,24));
 }
--- p3final.c
+++ p3final.c
@@ -1,48 +1,34 @@
-/*
-Write a program find whether a given number is a prime number.
-int input_number();
-int is_prime(int n);
-void output(int n, int is_prime);
-*/
-
-#include <stdio.h>
-#include <math.h>
-int input()
+#include<stdio.h>
+int input_n_and_r(int *n, int *r) 
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+  printf("Enter n : ");
+  scanf("%d", n);
+  printf("Enter r : ");
+  scanf("%d", r);
 }
+int ncr(int n, int r) 
+{
+  int ans, num = 1, den = 1;
 
-int isprime(int n)
-{
-  if (n==0 || n==1) {
-    return 0
-  }
-  if (n==2) {
-    return 1;
-  }
-  for(int i= 2; i<= sqrt(n);i++)
-  {
-    if(n%i==0)
-      return 0;
-  }
-  return 1;
+  for (int i=1; i<=(n-r); i++) 
+    {
+    den = den*i;
+    }
+  for (int i=1; i<=n; i++)
+    {
+    num = num*i;
+    }
+  ans = num/den;
+  return ans;
 }
-void output(int n, int isp)
+void output(int n, int r, int result) 
 {
-  if(isp == 0)
-    printf("%d is not a prime number\n",n);
-  else
-    printf("%d is a prime number\n",n)
-}
-
-int main()
-{
-  int n = input();
-  int isp = isprime(n);
-  output(n,isp);
+  printf("%dC%d = %d\n", n, r, result);
+} 
+int main() {
+  int n, r, ans;
+  input_n_and_r(&n, &r);
+  ans = ncr(n, r);
+  output(n, r, ans);
   return 0;
-  }
 }
